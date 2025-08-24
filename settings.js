38bcd42cb38d6c80643342540e9c2b38e618f0f4
Yomidu1 <osfernando0407@gmail.com>
2025-08-24 23:34:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,6 +7,6 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=pZwQgLqT#mJuOGcyx7q8VA7dOf2RE9zeRPVlXReVie5QbY669gWA' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "yomidu" : process.env.SESSION_NAME,
+SESSION_NAME: process.env.PORT === undefined ? "fernando" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
 };
